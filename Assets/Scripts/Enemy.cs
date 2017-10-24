5671f8a20ec13b7f34846d2bb03d3155099353c9
James Conrad <james.conrad@uoit.net>
2017-10-24 14:08:52 -0400

    [*] Finished JSON Encounter loading
    Only thing left is scene transitions, floor increments, and enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,20 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+    
+    public JSONManager.EnemyJSON m_stats;
 
-    public struct EnemyJSON
-    {
-        public string type;    //used for spawning
-        public string aitype;  //aitype used
-        public float hp;       //max health
-        public float dmg;      //damage per swing
-        public float atsp;     //swings per second
-        public float radius;   //used for spawning, allows for custom packing
-    }
-
-    public EnemyJSON m_stats;
-
-    public void SetJSON(EnemyJSON j)
+    public void SetJSON(JSONManager.EnemyJSON j)
     {
         m_stats = j;
     }

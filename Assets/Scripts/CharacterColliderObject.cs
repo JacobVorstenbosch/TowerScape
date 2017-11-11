4f82087f55d9808a135f115e514ff3f47b5d7640
James Conrad <james.conrad@uoit.net>
2017-11-11 15:45:10 -0500

    [+] Collision Framework

---
+++ Assets/Scripts/CharacterColliderObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterColliderObject : MonoBehaviour {
+
+    private int hitLayer;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("THit");
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        print("CHit");
+    }
+}
---
+++ Assets/Scripts/CollisionTreeManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTreeManager : MonoBehaviour {
+
+    public Transform skeletonRef;
+    public Transform weaponHand;
+    public GameObject weapon;
+
+    private List<Collider> colliders = new List<Collider>();
+
+	// Use this for initialization
+	void Start ()
+    {
+        RecursiveFillColliderList(skeletonRef);
+        for (int i = 0; i < colliders.Count; i++)
+        {
+            colliders[i].isTrigger = true;
+            colliders[i].transform.gameObject.AddComponent<CharacterColliderObject>();
+        }
+
+        GameObject equipedWeapon = Instantiate(weapon, weaponHand);
+        equipedWeapon.layer = 10;
+	}
+
+    private void RecursiveFillColliderList(Transform node)
+    {
+        for (int i = 0; i < node.transform.childCount; i++)
+            RecursiveFillColliderList(node.GetChild(i));
+        Collider c = node.gameObject.GetComponent<Collider>();
+        if (c)
+            colliders.Add(c);
+    }
+}

0584791688c682b67aa8420757b0f9cdf27f15b0
Unknown <giordano.fuoco@uoit.net>
2017-11-13 11:04:24 -0500

    Movement WRT camera
    movemenet based off camera, rotation in code but buggy

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,66 +9,43 @@ public class PlayerScript : MonoBehaviour
     public float player_Speed = 100.0f;
     private float inputH;
     private float inputV;
-    private Vector3 move_direction;
+    Vector3 moveDir;
+    public float DegreesPerSecond = 60.0f;
+
     // Use this for initialization
     void Start()
     {
         anim = GetComponent<Animator>();
         rbody = GetComponent<Rigidbody>();
-        move_direction = new Vector3(0, 0, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        var camera = Camera.main;
+        var forward = camera.transform.forward;
+        var right = camera.transform.right;
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
         //Character Movement
         //getting input from controller left stick
         inputH = Input.GetAxis("LeftStickHorizontal");
         inputV = Input.GetAxis("LeftStickVertical");
-
-        Vector2 LStickInput = new Vector2(inputH, inputV).normalized;
-
+        // = Input.GetButtonDown("Fire3");//get x button press
         //animator setting values
         anim.SetFloat("inputH", inputH);
         anim.SetFloat("inputV", inputV);
         //rbody movement
-        float moveX = inputH * player_Speed * Time.deltaTime;
-        float moveZ = inputV * -player_Speed * Time.deltaTime;
-        rbody.velocity = new Vector3(moveX, 0.0f, moveZ);
+        // moveDir = new Vector3(inputH * player_Speed * Time.deltaTime, 0, inputV * -player_Speed * Time.deltaTime);
+        var desiredMoveDir = -forward * inputV + right * inputH;
+        transform.Translate(desiredMoveDir * player_Speed * Time.deltaTime);
+       // rbody.velocity = new Vector3(inputH *player_Speed * Time.deltaTime, 0.0f, inputV * -player_Speed * Time.deltaTime);
+        //rotate char
+       //transform.Rotate(0, Input.GetAxis("LeftStickHorizontal") * DegreesPerSecond * Time.deltaTime, 0);
 
-        MoveDir(LStickInput);
-
-
-
-
-       //testing
-        if (Input.GetKey("w"))
-        {
-            anim.SetFloat("inputV", 1);
-        }
-
-        if (Input.GetKey("s"))
-        {
-            anim.SetFloat("inputV", -1);
-        }
-
-        if (Input.GetKey("a"))
-        {
-            anim.SetFloat("inputH", -1);
-        }
-        if (Input.GetKey("d"))
-        {
-            anim.SetFloat("inputH", 1);
-        }
     }
 
-    void MoveDir(Vector3 joystick_Direction)
-    {
-        float sign = (move_direction.x < inputH) ? -1.0f : 1.0f;
-        float angle = Vector3.Angle(move_direction, joystick_Direction) * sign;
-
-        float step = 60.0f * (Time.deltaTime * 60.0f);
-        move_direction = Vector3.RotateTowards(move_direction, joystick_Direction, step, 0.0f);
-        move_direction.Normalize();
-    }
-}
+}  

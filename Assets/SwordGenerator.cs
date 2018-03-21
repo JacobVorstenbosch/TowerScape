b85bf6be14ee93e4c6b12848350b0eafdd848ed4
Unknown <giordano.fuoco@uoit.net>
2018-03-21 12:53:51 -0400

    [+] Sword Gen wooo
    next is figuring out which components to use and then shaders and mats on them kk thx

---
+++ Assets/SwordGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordGenerator: MonoBehaviour {
+
+    public GameObject[] HiltComponents;
+    public GameObject[] GuardComponents;
+    public GameObject[] BladeComponents;
+
+    public void SwordGeneratorC(int hiltIndex, int guardIndex, int bladeIndex )
+    {
+        GenerateHiltComponent(hiltIndex);
+        GenerateGuardComponent(guardIndex);
+        GenerateBladeComponent(bladeIndex);
+    }
+    
+    private void GenerateHiltComponent(int componentIndex)
+    {
+        Instantiate(HiltComponents[componentIndex], transform);
+    }
+    private void GenerateGuardComponent(int componentIndex)
+    {
+        Instantiate(GuardComponents[componentIndex], transform);
+    }
+    private void GenerateBladeComponent(int componentIndex)
+    {
+        Instantiate(BladeComponents[componentIndex], transform);
+    }
+
+    void Start()
+    {
+        SwordGeneratorC(0, 0, 0);
+    }
+
+}

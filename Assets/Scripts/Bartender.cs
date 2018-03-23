275921d0561e3b88183ead2021061980e55d8936
James Conrad <james.conrad@uoit.net>
2018-03-23 10:29:22 -0400

    [+] Bar needed a Bartender

---
+++ Assets/Scripts/Bartender.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bartender : MonoBehaviour {
+
+    bool active = false;
+    float cooldown = 0.0f;
+    private Health playerHp;
+
+    public InteractionPane pane;
+
+	// Use this for initialization
+	void Start () {
+        playerHp = GameObject.FindGameObjectWithTag("PlayerRoot").GetComponent<Health>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cooldown -= Time.deltaTime;
+		if (active && cooldown <= 0 && Input.GetAxis("Fire1") > 0.1f)
+        {
+            cooldown = 1.0f;
+            if (playerHp.currentHealth > 1)
+                playerHp.currentHealth -= 10;
+            else
+                playerHp.currentHealth = 1;
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("PlayerRoot"))
+        {
+            active = true;
+            pane.SetActive(true);
+            pane.SetText("Press X to order a drink.");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("PlayerRoot"))
+        {
+            active = false;
+            pane.SetText(" ");
+            pane.SetActive(false);
+        }
+    }
+}

2c7b5558de9fb0104344841e16c35c6afcc02915
Unknown <giordano.fuoco@uoit.net>
2018-03-23 10:42:45 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/SlimeColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeColor : MonoBehaviour {
+
+    private Vector4 color = new Vector4(0, 0, 0, 0);
+
+    // Use this for initialization
+    void Start () {
+        float ranVal = Random.Range(0, 255);
+        Color c = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+        color = new Vector4(c.r, c.g, c.b, c.a);
+        GetComponent<Renderer>().material.SetColor("_Color", color);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
